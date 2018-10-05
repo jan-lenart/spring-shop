38a9589b2ab63d1042bad9529e61b1085e3e2640
Jan Lenart <johny142@gmail.com>
2018-10-05 10:45:28 +0200

    Added equals and hash code to entities

--- src/main/java/com/janlenart/springshop/bo/domain/Customer.java
+++ src/main/java/com/janlenart/springshop/bo/domain/Customer.java
@@ -22,4 +22,27 @@ public class Customer {
     @OneToOne(cascade = CascadeType.ALL)
     private Address shippingAddress;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (id != customer.id) return false;
+        if (!name.equals(customer.name)) return false;
+        if (!surname.equals(customer.surname)) return false;
+        if (!pesel.equals(customer.pesel)) return false;
+        return shippingAddress.equals(customer.shippingAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + surname.hashCode();
+        result = 31 * result + pesel.hashCode();
+        result = 31 * result + shippingAddress.hashCode();
+        return result;
+    }
 }
--- src/main/java/com/janlenart/springshop/bo/domain/Item.java
+++ src/main/java/com/janlenart/springshop/bo/domain/Item.java
@@ -27,4 +27,34 @@ public class Item {
     @JsonBackReference
     @ManyToOne
     private OrderInfo orderInfo;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (id != item.id) return false;
+        if (Float.compare(item.price, price) != 0) return false;
+        if (quantity != item.quantity) return false;
+        if (!name.equals(item.name)) return false;
+        if (!description.equals(item.description)) return false;
+        if (!barcode.equals(item.barcode)) return false;
+        if (!priceCurrency.equals(item.priceCurrency)) return false;
+        return orderInfo.equals(item.orderInfo);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + description.hashCode();
+        result = 31 * result + barcode.hashCode();
+        result = 31 * result + (price != +0.0f ? Float.floatToIntBits(price) : 0);
+        result = 31 * result + quantity;
+        result = 31 * result + priceCurrency.hashCode();
+        result = 31 * result + orderInfo.hashCode();
+        return result;
+    }
 }
